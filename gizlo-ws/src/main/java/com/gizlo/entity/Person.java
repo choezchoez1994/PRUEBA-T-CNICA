d8bb1e3089870dd9ea460750f687b53da8233893
jonathan choez <choez552@gmail.com>
2022-02-03 12:54:51 -0500

    rama v1

---
+++ gizlo-ws/src/main/java/com/gizlo/entity/Person.java
@@ -0,0 +1,80 @@
+package com.gizlo.entity;
+
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "person")
+public class Person {
+
+    @BsonId
+    private String id;
+    private String name;
+    private String lastname;
+    private String identification;
+    private String direccion;
+    private String telefono;
+    private String ciudad;
+
+    public Person() {
+    }
+
+    public Person(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+}
---
+++ gizlo-ws/src/main/java/com/gizlo/resource/PersonResource.java
@@ -0,0 +1,57 @@
+package com.gizlo.resource;
+
+import com.gizlo.entity.Person;
+import com.gizlo.service.IPersonService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class PersonResource {
+
+    Logger logger = LoggerFactory.getLogger(PersonResource.class);
+
+    @Autowired
+    private IPersonService _service;
+
+    @PostMapping("savePerson")
+    public ResponseEntity<?> savePerson(@RequestBody Person person) {
+        try {
+            return new ResponseEntity<>(_service.save(person), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("findAllPerson")
+    public ResponseEntity<?> findAllPerson() {
+        try {
+            return new ResponseEntity<>(_service.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("updatePerson")
+    public ResponseEntity<?> updatePerson(@RequestBody Person person) {
+        try {
+            return new ResponseEntity<>(_service.update(person), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("flushCache")
+    public void flushCache() {
+        logger.info("flushCache");
+        _service.flushCache();
+    }
+}
